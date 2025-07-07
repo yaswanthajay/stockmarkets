a446b379e62f0782c14a8d2e8b2293ea8d538e49
yaswanthajay <153178937+yaswanthajay@users.noreply.github.com>
2025-07-07 15:19:37 +0530

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def create_lstm_dataset(data, feature_cols, target_col='Close', seq_length=30):
+def create_lstm_dataset(df, feature_cols, target_col, look_back=20):
     X, y = [], []
-    for i in range(len(data) - seq_length):
-        X.append(data[feature_cols].iloc[i:i+seq_length].values)
-        y.append(data[target_col].iloc[i+seq_length])
+    for i in range(look_back, len(df)):
+        X.append(df[feature_cols].iloc[i-look_back:i].values)
+        y.append(df[target_col].iloc[i])
     return np.array(X), np.array(y)
