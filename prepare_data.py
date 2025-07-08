7a98f3b17293d066f75fc143f7a9ef2b5e3ae417
yaswanthajay <153178937+yaswanthajay@users.noreply.github.com>
2025-07-08 13:59:32 +0530

    Delete prepare_data.py

--- prepare_data.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def create_lstm_dataset(df, feature_cols, target_col, look_back=20):
-    X, y = [], []
-    for i in range(look_back, len(df)):
-        X.append(df[feature_cols].iloc[i-look_back:i].values)
-        y.append(df[target_col].iloc[i])
-    return np.array(X), np.array(y)
