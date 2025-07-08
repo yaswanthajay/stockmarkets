05f38744b01b422e905eae72141818238ec0009d
yaswanthajay <153178937+yaswanthajay@users.noreply.github.com>
2025-07-08 22:31:06 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,14 +1,13 @@
 from tensorflow.keras.models import load_model
-import numpy as np
 import joblib
+import numpy as np
 
 def load_trained_model():
-    return load_model("my_model.h5")
+    model = load_model("my_model.h5")
+    scaler = joblib.load("scaler.save")
+    return model, scaler
 
-def load_scaler():
-    return joblib.load("scaler.save")
-
-def predict(data_dict, model):
+def predict(data_dict, model, scaler):
     X = data_dict['scaled_input'][-1].reshape(1, 60, 1)
-    prediction = model.predict(X, verbose=0)
-    return data_dict['scaler'].inverse_transform(prediction)[0][0]
+    prediction = model.predict(X)
+    return scaler.inverse_transform(prediction)[0][0]
