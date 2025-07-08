f02db7c30af5e4df577add6988f58f5e79d687d8
yaswanthajay <153178937+yaswanthajay@users.noreply.github.com>
2025-07-08 13:58:40 +0530

    Delete explanability.py

--- explanability.py
+++
@@ -1,7 +0,0 @@
-import shap
-import matplotlib.pyplot as plt
-
-def explain(model, X_sample):
-    explainer = shap.DeepExplainer(model, X_sample[:100])
-    shap_values = explainer.shap_values(X_sample[:100])
-    shap.summary_plot(shap_values, X_sample[:100])
